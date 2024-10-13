ecb2c8b635422c2ed31a5ed49b51773c868e8932
poli-maria <molokotyan@gmail.com>
2024-10-13 22:24:22 +0300

    Add files via upload

---
+++ module 2.3.py
@@ -0,0 +1,12 @@
+i = 0
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5,]
+
+while i < len(my_list):
+    if my_list[i] == 0:
+        i += 1
+        continue
+        if my_list[i] <= -1:
+            break
+    print(my_list[i])
+    i += 1
+
